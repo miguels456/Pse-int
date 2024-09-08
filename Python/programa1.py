97d8492d394e5dd1926d31e00f6b95c997e7f925
Miguel4560_0 <109480379+miguels456@users.noreply.github.com>
2024-09-07 20:16:29 -0600

    Add files via upload

---
+++ Python/programa1.py
@@ -0,0 +1,17 @@
+print("este programa suma y resta numeros")
+num1=float(input("ingresa tu primer numero: "))
+num2=float(input("ingresa tu segundo numero: "))
+
+suma= num1+num2
+resta= num1-num2
+print(f"el resultado de la suma de los 2 numeros es:,{suma}!")
+print(f"el resultado de la resta de los 2 numeros es:,{resta}!")
+
+
+
+
+      
+
+
+
+# python
